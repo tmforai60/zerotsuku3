7bbbd66da1d5d823762b5f0ddfd891cb766f9437
Daisuke Nagao <numrecipes@gmail.com>
2022-09-18 17:06:09 +0900

    step33-36 created by Daisuke Nagao

---
+++ chapter3/step33~36/dezero/__init__.py
@@ -0,0 +1,26 @@
+# =============================================================================
+# step23.pyからstep32.pyまではsimple_coreを利用
+is_simple_core = False  # True
+# =============================================================================
+
+if is_simple_core:
+    from dezero.core_simple import Variable
+    from dezero.core_simple import Function
+    from dezero.core_simple import using_config
+    from dezero.core_simple import no_grad
+    from dezero.core_simple import as_array
+    from dezero.core_simple import as_variable
+    from dezero.core_simple import setup_variable
+
+else:
+    from dezero.core import Variable
+    from dezero.core import Function
+    from dezero.core import using_config
+    from dezero.core import no_grad
+    from dezero.core import as_array
+    from dezero.core import as_variable
+    from dezero.core import setup_variable
+    from dezero.core import Config
+
+setup_variable()
+__version__ = '0.0.13'
